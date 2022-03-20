a249c2ccb5ddd5f1b789c47816f03243406026be
Koehler <ckoehler16@gmail.com>
2022-03-20 18:47:08 -0400

    random fight order

--- assets/js/game.js
+++ assets/js/game.js
@@ -38,17 +38,25 @@ var fightOrSkip = function() {
 
 // fight function (now with parameter for enemy's object holding name, health, and attack values)
 var fight = function(enemy) {
+  // keep track of who goes first
+  var isPlayerTurn = true;
+
+  // randomly change turn order
+  if (Math.random() > 0.5) {
+    isPlayerTurn = false;
+  }
+
   while (playerInfo.health > 0 && enemy.health > 0) {
-    // ask player if they'd like to fight or run
+    if (isPlayerTurn) {
+    // ask player if they'd like to fight or skip using fightOrSkip
      if (fightOrSkip()) {
        //if true, leave fight by breaking loop
        break;
      }
-  }
 
-    // generate random damage value based on player's attack power
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
+    // remove enemy's health by subtracking the amount we set in the damage variable
     enemy.health = Math.max(0, enemy.health - damage);
     console.log(
       playerInfo.name + ' attacked ' + enemy.name + '. ' + enemy.name + ' now has ' + enemy.health + ' health remaining.'
@@ -62,9 +70,12 @@ var fight = function(enemy) {
       playerInfo.money = playerInfo.money + 20;
 
       // leave while() loop since enemy is dead
+      break;
     } else {
       window.alert(enemy.name + ' still has ' + enemy.health + ' health left.');
     }
+    // player gets attacked first
+  } else {
 
     // remove players's health by subtracting the amount set in the enemy.attack variable
     var damage = randomNumber(enemy.attack - 3, enemy.attack);
@@ -79,9 +90,14 @@ var fight = function(enemy) {
     if (playerInfo.health <= 0) {
       window.alert(playerInfo.name + ' has died!');
       // leave while() loop if player is dead
+      break;
     } else {
       window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
     }
+  }
+  // switch turn order for next round
+  isPlayerTurn = !isPlayerTurn;
+}
 };
 
 // function to start a new game
